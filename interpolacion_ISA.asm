fbdf2f846c03614c41f83a4cd470ce9e92470515
Calquito <39011234+Calquito@users.noreply.github.com>
2022-05-13 10:13:10 -0600

    Fix loads

--- interpolacion_ISA.asm
+++ interpolacion_ISA.asm
@@ -1,147 +1,605 @@
 //a
-ld r1 r2 0 
-ld r2 3
+ld r1 1 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+ld r2 4
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 mult r1 r1 2
-div r1 r1 3
 
-div r2 r2 3
-
-add r3 r2 r1
-    
-str r3 1
-
-//b
-//doesn't require to load again
-
-mult r2 r2 2
-div r2 r2 3
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 div r1 r1 3
 
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
 add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
     
 str r3 2
 
-//c
-ld r1 0 
-ld r2 3
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
-mult r1 r1 2
-div r1 r1 3
+//b
+ld r1 1 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 4
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+mult r2 r2 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 div r2 r2 3
 
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
 add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
     
 str r3 4
 
-//g
-mult r2 r2 2
-div r2 r2 3
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
-div r1 r1 3
+//c
+ld r1 1 
 
-add r3 r2 r1
-    
-str r3 8
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
-//k
-ld r1 3 
-ld r2 6
+ld r2 13
 
-mult r1 r1 2
-div r1 r1 3
-
-div r2 r2 3
-
-add r3 r2 r1
-    
-str r3 13
-
-//l
-
-mult r2 r2 2
-div r2 r2 3
-
-div r1 r1 3
-
-add r3 r2 r1
-    
-str r3 14
-
-//f
-ld r1 3 
-ld r2 6
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 mult r1 r1 2
-div r1 r1 3
 
-div r2 r2 3
-
-add r3 r2 r1
-    
-str r3 7
-
-//j
-
-mult r2 r2 2
-div r2 r2 3
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 div r1 r1 3
 
-add r3 r2 r1
-    
-str r3 11
-
-
-//d
-ld r1 7 
-ld r2 4
-
-mult r1 r1 2
-div r1 r1 3
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 div r2 r2 3
 
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
 add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
     
 str r3 5
 
-//e
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+//g
+
+ld r1 1 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 13
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 mult r2 r2 2
-div r2 r2 3
 
-div r1 r1 3
-
-add r3 r2 r1
-    
-str r3 6
-
-//h
-ld r1 2 
-ld r2 8
-
-mult r1 r1 2
-div r1 r1 3
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 div r2 r2 3
 
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
 add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
     
 str r3 9
 
-//i
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
-mult r2 r2 2
-div r2 r2 3
+//k
+ld r1 13 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 16
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+mult r1 r1 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
 
 div r1 r1 3
 
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
 add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 14
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+//l
+
+ld r1 13 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 16
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+mult r2 r2 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 15
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+//f
+ld r1 4 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 16
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+mult r1 r1 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 8
+
+//j
+
+ld r1 4 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 16
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+mult r2 r2 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 12
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+//d
+ld r1 5 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 8
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+mult r1 r1 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 7
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+//e
+
+ld r1 5 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 8
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+mult r2 r2 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 7
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+//h
+ld r1 9 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 12
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+mult r1 r1 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
     
 str r3 10
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+//i
+
+ld r1 9 
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+ld r2 12
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+
+mult r2 r2 2
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r2 r2 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+div r1 r1 3
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+
+add r3 r2 r1
+
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+add r10 r10 0
+    
+str r3 11
 
 
 
